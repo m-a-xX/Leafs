18298b39793c970f3ebcd387449dfd9e31519bec
m-a-xX <maxence.vileo@orange.fr>
2019-06-13 13:44:52 +0200

    Code refactoring

--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,20 @@
+"""Fil that contains views use in urls"""
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
+    """Homepage view"""
     return render(request, 'index.html')
 
 def credits(request):
+    """Legal mentions view"""
     return render(request, 'credits.html')
 
 def leafs(request):
+    """Leafs view"""
     return render(request, 'leafs.html')
 
 def qcm(request):
+    """QCM view"""
     return render(request, 'qcm.html')
